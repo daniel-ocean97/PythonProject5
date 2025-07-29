e864ca89abd083e139470ea6b3fc44d21333d5cd
Даниил Ермилов <yndx-daniel-ocean@yandex.ru>
2025-07-29 10:51:06 +0300

    add custom command populate_groups.py

--- config/settings.py
+++ config/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PythonProject5',
+        'USER': 'postgres',
+        'PASSWORD': '3228',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
